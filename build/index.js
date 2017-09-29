3521a91af8697a8e17f5b4ea1ee9c2709fec3748
danieljdufour <daniel.j.dufour@gmail.com>
2017-09-29 01:21:33 +0000

    made compliant with Node versions where padStart and repeat don't exist

--- build/index.js
+++ build/index.js
@@ -4,7 +4,12 @@ function getUint2(byteOffset, littleEndian) {
     var floored = Math.floor(byteOffset);
     var bitOffset = byteOffset % 8 / 0.25;
     var raw_string = this.getUint8(floored).toString(2);
-    var padded_string = "0".repeat(8 - raw_string.length) + raw_string;
+    var padded_string = "";
+    var number_of_zeros = 8 - raw_string.length;
+    for (var i = 0; i < number_of_zeros; i++) {
+        padded_string += "0";
+    }
+    padded_string += raw_string;
     var bits_as_string = padded_string.substring(bitOffset, bitOffset + 2);
     return parseInt(bits_as_string, 2);
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ function getUint2(byteOffset, littleEndian) {
     let floored = Math.floor(byteOffset);
     let bitOffset = byteOffset % 8 / 0.25;
     let raw_string = this.getUint8(floored).toString(2);
-    let padded_string = "0".repeat(8 - raw_string.length) + raw_string;
+    let padded_string = "";
+    let number_of_zeros = 8 - raw_string.length;
+    for (let i = 0; i < number_of_zeros; i++) {
+        padded_string += "0";
+    }
+    padded_string += raw_string;
     let bits_as_string = padded_string.substring(bitOffset, bitOffset + 2);
     return parseInt(bits_as_string, 2);
 }
